5c655e6dd4e2444f93ed178639a7505fcddfc645
anastasiyatimofeyeva <ASTEEGHS@BETHEL.JW.ORG>
2025-05-30 19:24:51 +0200

    finish first steps

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import {
   ProductDetailComponent,
   ProductOverviewComponent,
 } from './pages';
+import { ComingSoonComponent } from './pages/coming-soon';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -14,6 +15,8 @@ const routes: Routes = [
   { path: 'product/:id', component: ProductDetailComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'furniture', component: ComingSoonComponent },
+  { path: 'services', component: ComingSoonComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
 
---
+++ src/app/pages/coming-soon/coming-soon.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coming-soon',
+  templateUrl: './coming-soon.component.html',
+  styleUrls: ['./coming-soon.component.scss'],
+  standalone: true,
+  imports: [CommonModule],
+})
+export class ComingSoonComponent {}
